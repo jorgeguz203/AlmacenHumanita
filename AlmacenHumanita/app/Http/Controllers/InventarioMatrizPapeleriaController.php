643864826afb980cfa5552a30bbf3f6141c28df7
JORGE ARMANDO GUZMAN FLORES <A01208480@itesm.mx>
2017-07-27 20:40:23 -0500

    buscadores terminados

--- AlmacenHumanita/app/Http/Controllers/InventarioMatrizPapeleriaController.php
+++ AlmacenHumanita/app/Http/Controllers/InventarioMatrizPapeleriaController.php
@@ -11,42 +11,42 @@ class InventarioMatrizPapeleriaController extends Controller
     
     	public function index1(Request $request)
     {
-        $materiales = MaterialPapelera::orderBy('nombre','ASC')->paginate(100);
+        $materiales = MaterialPapelera::name($request->get('name'))->orderBy('nombre','ASC')->paginate(100);
         return view('inventarioMatrizPapeleria.index1',compact('materiales'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
 
         	public function index2(Request $request)
     {
-        $materiales = MaterialPapelera::orderBy('nombre','ASC')->paginate(100);
+        $materiales = MaterialPapelera::name($request->get('name'))->orderBy('nombre','ASC')->paginate(100);
         return view('inventarioMatrizPapeleria.index2',compact('materiales'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
 
         	public function index3(Request $request)
     {
-        $materiales = MaterialPapelera::orderBy('nombre','ASC')->paginate(100);
+        $materiales = MaterialPapelera::name($request->get('name'))->orderBy('nombre','ASC')->paginate(100);
         return view('inventarioMatrizPapeleria.index3',compact('materiales'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
 
         	public function index4(Request $request)
     {
-        $materiales = MaterialPapelera::orderBy('nombre','ASC')->paginate(100);
+        $materiales = MaterialPapelera::name($request->get('name'))->orderBy('nombre','ASC')->paginate(100);
         return view('inventarioMatrizPapeleria.index4',compact('materiales'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
 
         	public function index5(Request $request)
     {
-        $materiales = MaterialPapelera::orderBy('nombre','ASC')->paginate(100);
+        $materiales = MaterialPapelera::name($request->get('name'))->orderBy('nombre','ASC')->paginate(100);
         return view('inventarioMatrizPapeleria.index5',compact('materiales'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
 
         	public function index6(Request $request)
     {
-        $materiales = MaterialPapelera::orderBy('nombre','ASC')->paginate(100);
+        $materiales = MaterialPapelera::name($request->get('name'))->orderBy('nombre','ASC')->paginate(100);
         return view('inventarioMatrizPapeleria.index6',compact('materiales'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
